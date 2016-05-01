844a3d9ebb80b600d7f5a3789eea96cb748f2df5
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-05-01 17:22:02 +0900

    プロフィール画面実装準備
    ビュー追加

---
+++ Near/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Near
+//
+//  Created by 堀江健太朗 on 2016/05/01.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.orangeColor()
+    }
+}
