14137e87d00948056d59d58696f16f334148ba10
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-05-01 17:20:40 +0900

    タイムライン用セル追加

---
+++ Near/MainTimeLineViewTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MainTimeLineViewTableViewCell.swift
+//  Near
+//
+//  Created by 堀江健太朗 on 2016/05/01.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class MainTimeLineViewTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userAvatarIMageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setUP()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setUP() {
+        userAvatarIMageView.layer.cornerRadius = userAvatarIMageView.frame.width / 2
+        userAvatarIMageView.clipsToBounds = true
+        self.layer.borderColor = UIColor.grayColor().CGColor
+    }
+    
+}
