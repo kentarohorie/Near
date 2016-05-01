d85c42b45f4067ddf7e56c25fd99440e8a1ebade
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-05-01 17:21:26 +0900

    メッセージのビュー追加

---
+++ Near/MessageListView.swift
@@ -0,0 +1,24 @@
+//
+//  MessageListView.swift
+//  Near
+//
+//  Created by 堀江健太朗 on 2016/05/01.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class MessageListView: UIView {
+
+  
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func awakeFromNib() {
+        setUP()
+    }
+    
+    func setUP() {
+        tableView.registerNib(UINib(nibName: "MainTimeLineViewTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTimeLineViewTableViewCell")
+    }
+
+}
