06bedf8196ddb02ea5240efcd16638ddf9356539
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-05-10 23:09:56 +0900

    メッセージ画面ビュー追加
    未使用

---
+++ Near/MessageView.swift
@@ -0,0 +1,17 @@
+//
+//  MessageView.swift
+//  Near
+//
+//  Created by 堀江健太朗 on 2016/05/10.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class MessageView: UIView {
+
+    override func awakeFromNib() {
+        self.backgroundColor = UIColor.redColor()
+    }
+
+}
