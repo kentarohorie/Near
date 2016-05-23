7e528aca96546f0bce3aaecafa549168b9d4e281
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-05-23 15:18:13 +0900

    について反映

--- Near/ProfileViewController.swift
+++ Near/ProfileViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    let profileVM = ProfileViewModel()
+    var user: User? = nil
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -20,8 +20,9 @@ class ProfileViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
-    convenience init() {
+    convenience init(user: User) {
         self.init(nibName: nil, bundle: nil)
+        self.user = user
         setUP()
     }
     
@@ -33,12 +34,42 @@ class ProfileViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    func setUP() {
+    private func setUP() {
         let profileV = UINib(nibName: "ProfileView", bundle: nil).instantiateWithOwner(self, options: nil).first as! ProfileView
         self.view = profileV
+        
+        guard let user = self.user else {
+            return
+        }
+        let profileVM = ProfileViewModel(user: user)
+        profileVM.delegate = profileV
+        profileVM.setUserToView()
     }
+    
+    
+    var navBar: UINavigationBar!
+    
+    override func didMoveToParentViewController(parent: UIViewController?) {
+        if User.currentUser().userName == self.user!.userName {
+            
+        } else {
+        guard parent?.getClassName() == nil else {
+            navBar = (parent as! UINavigationController).navigationBar
+            return
+        }
+        for i in navBar.subviews {
+            if let navView = i as? NearNavigationView {
+                for v in navView.subviews {
+                    v.userInteractionEnabled = true
+                    v.hidden = false
+                }
+                navView.hidden = false
+            }
+        }
+        }
+    }
+
     
 }
