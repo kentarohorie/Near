f7f883aa2a2d0ac8413dfa48720c137c187fa89c
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-06-10 14:07:00 +0900

    FBLoginViewのセット

--- Near/GeneralViewController.swift
+++ Near/GeneralViewController.swift
@@ -7,24 +7,39 @@
 //
 
 import UIKit
-import FBSDKCoreKit 
+import FBSDKLoginKit
 
 class GeneralViewController: UIViewController {
     
     private let pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
     private let generalVM = GeneralViewModel()
+    private let fbLoginVM = FBLoginViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setPageViewController()
-        setNavigationBar()
         
-        User.sampleSetUP()
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            print("already login")
+        } else {
+            print("yet")
+            setFBLoginView {
+                //            self.setPageViewController()
+                //            self.setNavigationBar()
+                
+                //            User.sampleSetUP()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func setFBLoginView(callback: () -> Void) {
+        let fbLoginView = FBLoginView(frame: view.frame, delegate: fbLoginVM)
+        self.view.addSubview(fbLoginView)
+        
+        callback()
     }
     
     //============ set pageviewcontroller ==========
