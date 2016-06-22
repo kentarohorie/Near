6905ce0c967d4f6c99483183e9ad3dd9c9c5b9f5
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-06-22 17:40:13 +0900

    updateAPIにログイン時間を追加

--- Near/GeneralViewController.swift
+++ Near/GeneralViewController.swift
@@ -41,7 +41,7 @@ class GeneralViewController: UIViewController, FBLoginViewModeldelegate, General
         if (FBSDKAccessToken.currentAccessToken() != nil) {
             print("already login")
             User.fetchFromAPI({
-                User.updateUserWithAPI(User.currentUser.age!, name: User.currentUser.userName!, latitude: User.coordinate[0], longitude: User.coordinate[1], callback: {
+                User.updateUserWithAPI(User.currentUser.age!, name: User.currentUser.userName!, latitude: User.coordinate[0], longitude: User.coordinate[1], loginTime: User.currentUser.loginTime!, callback: {
                     User.getUsersForTimelineAPI({
                         self.setPageViewController()
                         self.setNavigationBar()
