e3d1df387fc7e29f0d2579284d8e2b6a53ef925e
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-06-27 11:11:31 +0900

    aws画像保存

--- Near/ProfileEditViewModel.swift
+++ Near/ProfileEditViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AWSS3
 
 @objc protocol ProfileEditViewModelDelegate {
     func profileEditVM(didChangeImage sender: NSObject)
@@ -18,7 +19,48 @@ class ProfileEditViewModel: NSObject, ProfileEditViewDelegate, UIImagePickerCont
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         User.currentUser.avatar = image
         delegate?.profileEditVM(didChangeImage: self)
+        // カットした状態の画像をカットしたものとして受け取る工夫がいる。
+        //==upload image to s3 test
+        let uploadFileURL = editingInfo![UIImagePickerControllerReferenceURL] as! NSURL
+        let imageName = uploadFileURL.lastPathComponent
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first! as String
+        
+        // getting local path
+        let localPath = (documentDirectory as NSString).stringByAppendingPathComponent(imageName!)
+        
+        
+        //getting actual image
+        let image = editingInfo![UIImagePickerControllerOriginalImage] as! UIImage
+        let data = UIImagePNGRepresentation(image)
+        data!.writeToFile(localPath, atomically: true)
+        
+        let imageData = NSData(contentsOfFile: localPath)!
+        let imageURL = NSURL(fileURLWithPath: localPath)
+        
+        let S3BucketName: String = "nearfornearinc"
+        let S3UploadKeyName: String = "public/fuga.png"
+        
+        let transferUtility = AWSS3TransferUtility.defaultS3TransferUtility()
+        let expression = AWSS3TransferUtilityUploadExpression()
+
+        transferUtility.uploadFile(imageURL, bucket: S3BucketName, key: S3UploadKeyName, contentType: "image/png", expression: expression, completionHander: nil).continueWithBlock { (task) -> AnyObject! in
+            if let error = task.error {
+                print("Error: \(error.localizedDescription)")
+            }
+            if let exception = task.exception {
+                print("Exception: \(exception.description)")
+            }
+            if let _ = task.result {
+                print("Upload Starting!")
+            }
+            
+            return nil;
+        }
+        
+        //=================================
+
         picker.dismissViewControllerAnimated(true, completion: nil)
     }
+    
 
 }
