b84608503c6ca7ca1ac0e654daebc4e53594c7a1
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-07-11 03:48:21 +0900

    change about message view, vc with implementing api

--- Near/MessageViewController.swift
+++ Near/MessageViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
+@objc protocol MessageViewControllerDelegate {
+    func messageViewController(sentMessage sender: LGChatController)
+}
+
 class MessageViewController: LGChatController, LGChatControllerDelegate {
 
-    let messageVM = MessageViewModel()
+    internal var myDelegate: MessageViewControllerDelegate?
+    
+    private let messageVM = MessageViewModel()
+    private var room: MessageRoom!
+    private var userMessages: [LGChatMessage] = []
+    private var opponentUser: User!
+    private var isFromProfile: Bool = false
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    convenience init(room: MessageRoom, opponentUser: User, isFromProfile: Bool) {
+        self.init(nibName: nil, bundle: nil)
+        self.messages = room.messages
+        self.opponentUser = opponentUser
+        self.isFromProfile = isFromProfile
+        self.room = room
+        setUP()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUP()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,43 +48,43 @@ class MessageViewController: LGChatController, LGChatControllerDelegate {
     }
     
 
-    func setUP() {
-//        let messageV = UINib(nibName: "MessageView", bundle: nil).instantiateWithOwner(self, options: nil).first as! MessageView
-//        self.view = messageV
-        launchChatController()
+    private func setUP() {
+        setController()
+        setUser()
     }
     
-    
-    func launchChatController() {
-        self.opponentImage = UIImage(named: "sample_user")
-//        self.title = "Anne Hathaway"
-        let helloWorld = LGChatMessage(content: "Hello", sentBy: .Opponent)
-        let helloWorld2 = LGChatMessage(content: "Where you are now?", sentBy: .Opponent)
-        self.messages = [helloWorld, helloWorld2]
+    private func setController() {
         self.delegate = self
-//        self.navigationController?.pushViewController(chatController, animated: true)
     }
     
-    // MARK: LGChatControllerDelegate
-    
-    func chatController(chatController: LGChatController, didAddNewMessage message: LGChatMessage) {
-        print("Did Add Message: \(message.content)")
+    private func setUser() {
+//        self.title = "Anne Hathaway"   文字が黒
+        self.opponentImage = opponentUser.avatar
     }
+    
+    //======== LGChatControllerDelegate ==============
     
     func shouldChatController(chatController: LGChatController, addMessage message: LGChatMessage) -> Bool {
-        /*
-         Use this space to prevent sending a message, or to alter a message.  For example, you might want to hold a message until its successfully uploaded to a server.
-         */
+        room.messages.append(LGChatMessage(content: message.content, sentBy: .User))
+        MessageRoomManager.createSenderMessageToRoomWithAPI(message.content, userID: User.currentUser.fbID!, roomID: room.roomID, callback: nil)
+        myDelegate?.messageViewController(sentMessage: self)
         return true
     }
+        
+    //========= back navigation action ==========
     
     var navBar: UINavigationBar!
     
     override func didMoveToParentViewController(parent: UIViewController?) {
+        guard !(isFromProfile) else {
+            return
+        }
+        
         guard parent?.getClassName() == nil else {
             navBar = (parent as! UINavigationController).navigationBar
             return
         }
+        
         for i in navBar.subviews {
             if let navView = i as? NearNavigationView {
                 for v in navView.subviews {
