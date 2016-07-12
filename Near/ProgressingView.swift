d805e491d7bd35c998167e62cbb822f7d9ac0d16
堀江健太朗 <kentaro.horie.4@gmail.com>
2016-07-12 13:52:03 +0900

    implement progress view & launch screnn

---
+++ Near/ProgressingView.swift
@@ -0,0 +1,13 @@
+//
+//  ProgressingView.swift
+//  Near
+//
+//  Created by 堀江健太朗 on 2016/07/12.
+//  Copyright © 2016年 kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ProgressingView: UIView {
+
+}
